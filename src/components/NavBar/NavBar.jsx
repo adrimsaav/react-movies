457b30825ca6aadc5d6a0348def635e39c0c2191
adrimsaav <adrimsaav@gmail.com>
2023-12-13 11:44:43 -0500

    finish part 1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
+  const NavBar = ({ user }) => {
   return (
     <nav>
       <Link to="/">Movies List Page</Link>
       &nbsp; | &nbsp;
-      <Link to="/movies/:movieName">Movie Detail</Link>
-      &nbsp; | &nbsp;
       <Link to="/actors">Actors</Link>
+      &nbsp; | &nbsp;
+      <span>Welcome, {user.username}!</span>
     </nav>
   );
+  }
 }

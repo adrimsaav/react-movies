181c6564f8b68974cd3ff28fc462413214fa71f4
adrimsaav <adrimsaav@gmail.com>
2023-12-14 18:14:04 -0500

    finish part 2 & 3

--- src/pages/MoviesListPage/MoviesListPage.jsx
+++ src/pages/MoviesListPage/MoviesListPage.jsx
@@ -3,7 +3,7 @@ import MovieCard from "../../components/MovieCard/MovieCard";
 export default function MoviesListPage({ movies }) {
   return (
     <>
-      <h1>MoviesListPage</h1>
+      <h1>Movies List Page</h1>
       {movies.map((movie) => {
         return <MovieCard movie={movie} />;
       })}
